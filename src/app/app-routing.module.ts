2dccc7a869b2dee4309a8d5fb386b3c9d5d49c90
MatinMomin1603 <matinmomin13@gmail.com>
2023-01-02 14:05:06 +0530

    Added new dev brach for hash url by Matin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,7 +40,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { useHash: false, preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { useHash: true, preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule],
   providers: [RouteResolver]
